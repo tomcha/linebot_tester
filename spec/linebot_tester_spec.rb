a5c3925cd498306fcfbfc094d2a9cebc4641047a
tomcha <tomcha@tomcha.net>
2016-08-05 19:15:03 +0900

    Postメソッドのテストを追加

--- spec/linebot_tester_spec.rb
+++ spec/linebot_tester_spec.rb
@@ -8,19 +8,26 @@ describe LinebotTester do
   def app
     @app ||= LinebotTester::Application
   end
-  
+
   describe "Application" do
     include Rack::Test::Methods
     describe 'レスポンスの精査' do
-      describe '/ へのアクセス' do
+      describe '/ へのGetアクセス' do
         before { get '/'}
         it 'Getアクセスした時、正常なHTTPアクセスコードが返ること' do
           expect(last_response.status).to eq 200
         end
       end
+
+      describe '/へのPostアクセス' do
+        before { post '/linebot/callback'}
+        it 'Postアクセスした時、正常なHTTPアクセスコードが返ること' do
+          expect(last_response.status).to eq 200
+        end
+      end
     end
   end
-#  it 'does something useful' do
-#    expect(false).to eq(true)
-#  end
+  #  it 'does something useful' do
+  #    expect(false).to eq(true)
+  #  end
 end
