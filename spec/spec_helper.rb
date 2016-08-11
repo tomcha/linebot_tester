b8fb13948b740617c474f1672c27e59f7994363e
tomcha <tomcha@tomcha.net>
2016-08-11 14:31:10 +0900

    rack/testを使う準備

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'linebot_tester'
 require 'linebot_tester/version'
 require 'rack/test'
+
+RSpec.configure do |conf|
+    conf.include Rack::Test::Methods
+end
